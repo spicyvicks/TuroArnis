e4ba68847fdba4ce54f9e75c335a4f94e8f50653
moonstarring <120117488+spicyvicks@users.noreply.github.com>
2025-10-26 03:19:13 +0800

    ugh

--- training/data_augmentation.py
+++ training/data_augmentation.py
@@ -5,8 +5,8 @@ import time
 import numpy as np
 from tqdm import tqdm #
 
-INPUT_DATASET_FOLDER = "dataset_multiclass_2"
-OUTPUT_DATASET_FOLDER = "dataset_multiclass"
+INPUT_DATASET_FOLDER = "cropped"
+OUTPUT_DATASET_FOLDER = "dataset"
 
 IMAGES_PER_ORIGINAL = 10 # images to create
 
